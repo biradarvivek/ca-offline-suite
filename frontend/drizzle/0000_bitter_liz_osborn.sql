b1485c27516c8725e7d7a3a5d0c306d3f3919c62
Harsh Jajal <99310495+HarshJ166@users.noreply.github.com>
2025-01-16 16:23:13 +0530

    Uat (#34)
    * Update README.md
    
    * first commit
    
    * electron fixes
    
    * bug fixes in package.json
    
    * fixed update of main.js
    
    * UI changes
    
    * UI Changes 2.0
    
    * change of sidebar
    
    * Sidebar Changes
    
    * changes in breadcrumbs and ui of ind. dash
    
    * breadcrumb change
    
    * eligibility ui change
    
    * Summary Data Changes
    
    * changes in db
    
    ---------
    
    Co-authored-by: Aiyaz17 <68592281+Aiyaz17@users.noreply.github.com>

--- frontend/drizzle/0000_bitter_liz_osborn.sql
+++ frontend/drizzle/0000_bitter_liz_osborn.sql
@@ -2,15 +2,15 @@ CREATE TABLE `cases` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` integer NOT NULL,
 	`status` text NOT NULL,
-	`created_at` text NOT NULL,
+	`created_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE CASCADE
 );
 --> statement-breakpoint
 CREATE TABLE `finance_vouchers` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`company_name` text NOT NULL,
-	`date` text NOT NULL,
-	`effective_date` text NOT NULL,
+	`date` integer NOT NULL,
+	`effective_date` integer NOT NULL,
 	`bill_reference` text NOT NULL,
 	`dr_ledger` text NOT NULL,
 	`cr_ledger` text NOT NULL,
@@ -29,14 +29,14 @@ CREATE TABLE `statements` (
 	`customer_name` text NOT NULL,
 	`ifsc_code` text,
 	`bank_name` text,
-	`created_at` text NOT NULL,
+	`created_at` integer NOT NULL,
 	FOREIGN KEY (`case_id`) REFERENCES `cases`(`id`) ON UPDATE no action ON DELETE CASCADE
 );
 --> statement-breakpoint
 CREATE TABLE `transactions` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`statement_id` text NOT NULL,
-	`date` text NOT NULL,
+	`date` integer NOT NULL,
 	`description` text NOT NULL,
 	`amount` real NOT NULL,
 	`category` text NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE `users` (
 	`name` text NOT NULL,
 	`email` text NOT NULL,
 	`password` text NOT NULL,
-	`date_joined` text NOT NULL
+	`date_joined` integer NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
